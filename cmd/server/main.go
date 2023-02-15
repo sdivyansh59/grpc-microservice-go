a2b6039867a5e2301e8f9496bdc9c8423de08e44
Divyansh Singh <sdivyansh59@gmail.com>
2023-02-15 13:02:51 +0530

    need to work on mock test{

---
+++ cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func Run() error {
+	// responsible for initialization and starting
+	// out gRPC server
+	return nil
+}
+
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("every thing fine!")
+}
